b8ae538fb12d6bdced5bbc356ed20118cef9b850
unknown <lyotin44@gmail.com>
2022-10-06 13:28:34 +0300

    Add types for Chat, User and Message

---
+++ src/utils/types.ts
@@ -0,0 +1,28 @@
+export interface User {
+	_id: string;
+	photo: string;
+	email: string;
+	username?: string;
+	createdAt?: string;
+	updatedAt?: string;
+}
+
+export interface Message {
+	_id: string;
+	sender: User;
+	content: string;
+	chat: string;
+	createdAt: string;
+	updatedAt: string;
+}
+
+export interface Chat {
+	_id: string;
+	chatName: string;
+	isGroupChat: boolean;
+	createdAt: string;
+	updatedAt: string;
+	users: User[];
+	groupAdmin: User;
+	latestMessage: Message;
+}
