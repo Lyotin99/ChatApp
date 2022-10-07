c99e49018afd53ff35304ead2e6115c7a7a939c1
unknown <lyotin44@gmail.com>
2022-10-07 14:37:03 +0300

    Create component: MessageItem

---
+++ src/components/MessageItem/MessageItem.tsx
@@ -0,0 +1,30 @@
+import { useAuthContext } from "../../context/AuthContext";
+import { Message } from "../../utils/types";
+
+interface MessageItemProps {
+	message: Message;
+}
+
+const MessageItem = ({
+	message: {
+		content,
+		createdAt,
+		sender: { _id: senderId },
+	},
+}: MessageItemProps) => {
+	const {
+		user: { userId },
+	} = useAuthContext();
+
+	return (
+		<div
+			className={`message ${senderId === userId ? "message--user" : ""}`}
+		>
+			<div className="message__inner">
+				<p>{content}</p>
+			</div>
+		</div>
+	);
+};
+
+export default MessageItem;
