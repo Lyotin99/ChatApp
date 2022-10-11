a5bd0e9e5ecd66250db196f1b1fa6ecf6d0e6f54
unknown <lyotin44@gmail.com>
2022-10-12 00:15:10 +0300

    Reformat UserServices code

--- src/services/UserServices.ts
+++ src/services/UserServices.ts
@@ -1,13 +1,8 @@
+import * as requester from "./requester";
 const usersURL = `http://localhost:5000/api/v1/users?search=`;
 
 const searchUsers = async (search: string, token: string) => {
-	const req = await fetch(`${usersURL}${search}`, {
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-	});
-
-	const res = await req.json();
+	const res = requester.get(`${usersURL}${search}`);
 
 	return res;
 };
